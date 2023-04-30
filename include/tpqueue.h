4c029d64df352f6b01da67c80991645ea56385a5
deprimiss <125207464+deprimiss@users.noreply.github.com>
2023-04-30 20:48:07 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,71 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+private:
+    struct Item {
+        T value;
+        Item* next;
+    };
+    Item* head;
+    Item* create(const T& value) {
+        Item* temp = new Item;
+        temp->value = value;
+        temp->next = nullptr;
+        return temp;
+    }
+
+public:
+    TPQueue() :head(nullptr) {}
+    void push(const T&);
+    T pop();
+};
+
+template<typename T>
+T TPQueue<T>::pop() {
+    if (head == nullptr) {
+        throw std::string("Empty!");
+    }
+    else {
+        T n  = head->value;
+        Item* tmp = head->next;
+        delete head;
+        head = tmp;
+        return tmp;
+    }
+}
+
+template<typename T>
+void TPQueue<T>::push(const T& n) {
+    if (head == nullptr) {
+        head = create(n);
+    } 
+    else {
+        Item* cur = head;
+        int i = 0;
+        while (cur) {
+            if (cur->value.prior < n.prior) {
+                break;
+            }
+            cur = cur->next;
+            i++;
+        }
+        if (i == 0) {
+            Item* tmp = new Item;
+            tmp->next = head;
+            tmp->value = n;
+            head = tmp;
+        }
+        else {
+            cur = head;
+            for (int i = 0; i < i; i++) {
+                cur = cur->next;
+            }
+            Item* tmp = new Item;
+            tmp->next = cur->next;
+            tmp->value = n;
+            cur->next = tmp;
+        }
+    }
 };
 
 struct SYM {
